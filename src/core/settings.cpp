2c7e2b8b54886313b12e953ae8679b87d8a85730
Alex Lemann <lemanal@gmail.com>
2025-12-29 09:15:56 -0500

    Check vector of alarms and alert in clock app

--- src/core/settings.cpp
+++ src/core/settings.cpp
@@ -1,8 +1,10 @@
 #include "settings.h"
+#include "../../lib/RTC/cplus_RTC.h"
 #include "core/led_control.h"
 #include "core/wifi/wifi_common.h"
 #include "display.h"
 #include "modules/ble_api/ble_api.hpp"
+#include "modules/others/audio.h"
 #include "modules/others/qrcode_menu.h"
 #include "modules/rf/rf_utils.h" // for initRfModule
 #include "mykeyboard.h"
@@ -885,10 +887,22 @@ void setClock() {
 
 void runClockLoop() {
     int tmp = 0;
-
 #if defined(HAS_RTC)
+    RTC_TimeTypeDef alarm;
+    alarm.Hours = 13;
+    alarm.Minutes = 05;
+    alarm.Seconds = 0;
+    RTC_TimeTypeDef previous_time;
     _rtc.GetBm8563Time();
     _rtc.GetTime(&_time);
+    Serial.println("RTC: Yes");
+#else
+    Serial.println("RTC: No");
+    tm alarm;
+    alarm.tm_hour = 14;
+    alarm.tm_min = 05;
+    alarm.tm_sec = 0;
+    tm time;
 #endif
 
     // Delay due to SelPress() detected on run
@@ -898,7 +912,8 @@ void runClockLoop() {
     for (;;) {
         if (millis() - tmp > 1000) {
 #if !defined(HAS_RTC)
-            updateTimeStr(rtc.getTimeStruct());
+            time = rtc.getTimeStruct();
+            updateTimeStr(time);
 #endif
             Serial.print("Current time: ");
             Serial.println(timeStr);
@@ -931,9 +946,17 @@ void runClockLoop() {
                 _time.Minutes % 100,
                 _time.Seconds % 100
             );
+            if (alarm.Hours == _time.Hours % 100 && alarm.Minutes == _time.Hours % 100) {
+                Serial.println("Alarm!!");
+            }
+            previous_time = _time;
             tft.drawCentreString(timeString, tftWidth / 2, tftHeight / 2 - 13, 1);
 #else
             tft.drawCentreString(timeStr, tftWidth / 2, tftHeight / 2 - 13, 1);
+            if (alarm.tm_hour == time.tm_hour && alarm.tm_min == time.tm_min && alarm.tm_sec == time.tm_sec) {
+                Serial.println("Alarm!!");
+                _tone(500, 5000);
+            }
 #endif
             tmp = millis();
         }
